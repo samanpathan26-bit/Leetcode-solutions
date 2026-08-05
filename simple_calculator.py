b149c94d8bd84c6b11da4356616622683ade6e42
Saman Pathan <samanpathan26@gmail.com>
2026-08-05 10:06:30 +0530

    Add files via upload
    Arithmetic operations

---
+++ simple_calculator.py
@@ -0,0 +1,15 @@
+a=int(input("enter value of a:"))
+b=int(input("enter value of b:"))
+op=input("enter op")
+if op=='+':
+    print(a+b)
+elif op=='-':
+    print(a-b)
+elif op=='*':
+    print(a*b)
+elif op=='/':
+    print(a/b)
+elif op=='%':
+    print(a%b)
+else:
+    print("default")
